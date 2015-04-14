112738395fdfdfb896fb18938a22cfa44e8663f0
Lindsey Jacks <linzjax@gmail.com>
2015-04-14 11:38:46 -0400

    Update CracklePop.js

--- CracklePop.js
+++ CracklePop.js
@@ -1,15 +1,15 @@
 function CracklePop(){
-  for (i=1; i<=100; i++) {
-    if(i % 5 == 0 && i % 3 == 0){
+  for (x=1; x<=100; x++) {
+    if(x % 5 == 0 && x % 3 == 0){
       console.log('CracklePop');
     }
-    else if (i % 5 == 0){
+    else if (x % 5 == 0){
       console.log("Pop");
     }
-    else if (i % 3 == 0){
+    else if (x % 3 == 0){
       console.log("Crackle");
     } else {
-      console.log(i);
+      console.log(x);
     }
   };
 };
